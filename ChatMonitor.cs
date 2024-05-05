d88e2e9a6e83c4feb5bb7687c36b3b05e75b0d42
RobAshby4 <robert.n.ashby@gmail.com>
2024-05-05 15:17:53 -0700

    Added multithreading for monitors

--- ChatMonitor.cs
+++ ChatMonitor.cs
@@ -1,25 +1,54 @@
+using System.Collections;
 using System.Threading;
 
-namespace LiveChatMultiplexer 
+namespace LiveChatMultiplexer
 {
 
-    enum Platform 
+    enum Platform
     {
-        Youtube,
+        YouTube,
         Twitch
     }
 
-    internal class ChatMonitor 
+    internal class ChatMonitor
     {
-        
+
         String chatURL;
         Platform chatPlatform;
+        private static Mutex writeMut = new Mutex();
+        private static ArrayList callbackList = new ArrayList();
 
         public ChatMonitor(Platform p, String url)
         {
-            this.chatPlatform = p; 
-            this.chatURL = url; 
+            this.chatPlatform = p;
+            this.chatURL = url;
         }
+
+        public static void setCallbackList(ArrayList callbackList)
+        {
+            writeMut.WaitOne();
+            ChatMonitor.callbackList = callbackList;
+            writeMut.ReleaseMutex();
+        }
+
+        public void runMonitor()
+        {
+            ArrayList updateList = new ArrayList();
+            Random rnd = new Random();
+            Thread.Sleep(rnd.Next(1, 1000));
+            updateList.Add(this.chatURL);
+            writeToCallback(updateList);
+        }
+
+        private void writeToCallback(ArrayList updates)
+        {
+            writeMut.WaitOne();
+            foreach (var c in updates) {
+                ChatMonitor.callbackList.Add(c);
+            }
+            writeMut.ReleaseMutex();
+        }
+
         // https://learn.microsoft.com/en-us/dotnet/standard/threading/creating-threads-and-passing-data-at-start-time
         // I need to implement callbacks for the chat monitor
     }
--- Program.cs
+++ Program.cs
@@ -1,24 +1,60 @@
 ﻿using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
 using System.Threading;
+using System.Collections;
 
-namespace LiveChatMultiplexer {
+namespace LiveChatMultiplexer
+{
 
-    internal class Program {
+    internal class Program
+    {
 
         public static void Main(String[] args)
         {
-            Console.WriteLine("Starting Multiplexer");
-            
-            var options = new ChromeOptions();
-            options.AddArgument("--headless=new");
-            var driver = new ChromeDriver(options);
+            ArrayList outList = new ArrayList();
+            ArrayList monitors = new ArrayList();
+            ArrayList threads = new ArrayList();
 
-            driver.Url = "https://www.google.com";
-            driver.FindElement(By.Name("q")).SendKeys("webdriver" + Keys.Return);
-            Console.WriteLine(driver.Title);
+            // generate all of the ChatMonitors
+            for (int i = 0; i < 100; i++)
+            {
+                monitors.Add(new ChatMonitor(Platform.Twitch, "Thread " + i.ToString()));
+            }
+            ChatMonitor.setCallbackList(outList);
 
-            driver.Quit();
+            // Give Each of them Threads and start them
+            foreach (ChatMonitor m in monitors)
+            {
+                Thread newThread = new Thread(new ThreadStart(m.runMonitor));
+                threads.Add(newThread);
+                newThread.Start();
+            }
+
+            // Join all threads
+            foreach (Thread t in threads)
+            {
+                t.Join();
+            }
+
+            // print the callback list
+            foreach (String output in outList)
+            {
+                Console.WriteLine(output);
+            }
+
+            Console.WriteLine(outList.Count);
+
+            // Console.WriteLine("Starting Multiplexer");
+
+            // var options = new ChromeOptions();
+            // options.AddArgument("--headless=new");
+            // var driver = new ChromeDriver(options);
+
+            // driver.Url = "https://www.google.com";
+            // driver.FindElement(By.Name("q")).SendKeys("webdriver" + Keys.Return);
+            // Console.WriteLine(driver.Title);
+
+            // driver.Quit();
         }
 
     }
